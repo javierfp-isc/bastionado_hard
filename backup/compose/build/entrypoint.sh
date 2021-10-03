cecb081ba1fd22c9358434c87a362faf37e6dd31
Javier Fernández Peón <javierfp@iessanclemente.net>
2021-10-03 21:26:53 +0200

    Escenario Backups

--- backup/compose/build/entrypoint.sh
+++ backup/compose/build/entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-exec /lib/systemd/systemd
+#Si existe el archivo /etc/routes lo ejecuta
+[ -f /etc/routes ] && sh /etc/routes
 
+#Cargamos iptables almacenadas si existe el archivo
+[ -f /etc/iptables/rules.v4 ] && iptables-restore < /etc/iptables/rules.v4
+
+#Si dispone de systemd lo arranca
+[ -f /lib/systemd/systemd ] && exec /lib/systemd/systemd
