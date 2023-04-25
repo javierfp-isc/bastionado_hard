e9dfb4b0d9a22f27988384c1d1d75a66ccd5adca
Javier Fernández Peón <javierfp@iessanclemente.net>
2023-04-25 17:10:05 +0200

    Migracion a Debian

--- backup/compose/build/entrypoint.sh
+++ backup/compose/build/entrypoint.sh
@@ -6,10 +6,10 @@
 #Cargamos iptables almacenadas si existe el archivo
 [ -f /etc/iptables/rules.v4 ] && iptables-restore < /etc/iptables/rules.v4
 
-#Si dispone de systemd lo arranca
-#[ -f /lib/systemd/systemd ] && exec /lib/systemd/systemd
-
 #Arranque para debian
+#Servidor
 [ -f /etc/init.d/urbackupsrv ] && /etc/init.d/urbackupsrv start
+#Cliente
+[ -f /etc/init.d/urbackupclientbackend ] && /etc/init.d/urbackupclientbackend start
 
 exec bash -c "while true;do sleep 10;done"
